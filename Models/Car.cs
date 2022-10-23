fa28f3ff96e0937bb7bf89ab84f756b61ed76d9a
rgruevski <robgruevski@gmail.com>
2022-10-23 12:41:33 -0400

    15. Create Data Access Services

--- Models/Car.cs
+++ Models/Car.cs
@@ -1,13 +1,16 @@
 ﻿namespace UdemyMAUICarListing.Models;
 public class Car
 {
-	public int Id { get; set; }
-	public string Make { get; set; }
-	public string Model { get; set; }
-	public string Vin { get; set; }
-	public Car()
-	{
-		
-	}
+    public int Id { get; set; }
+    public string Make { get; set; }
+    public string Model { get; set; }
+    public string Vin { get; set; }
+    public Car() { }
+    public Car(int id, string Make, string Model, string Vin)
+    {
+        this.Id = id;
+        this.Make = Make;
+        this.Model = Model;
+        this.Vin = Vin;
+    }
 }
-	
---
+++ Services/CarService.cs
@@ -0,0 +1,17 @@
+﻿using UdemyMAUICarListing.Models;
+
+namespace UdemyMAUICarListing.Services;
+public class CarService
+{
+    public static List<Car> GetCars() => new()
+    {
+        new Car(0,"Honda","Accord","1234567890"),
+        new Car(1,"Chevorlet","Corvette","1234567891"),
+        new Car(2,"Hyundai","Sonata","1234567892"),
+        new Car(3,"Ford","Focus","1234567893"),
+        new Car(4,"Nissan","Skyline","1234567894"),
+        new Car(5,"Acura","Integra","1234567895"),
+        new Car(6, "Mistubishi","Lancer","1234567896"),
+        new Car(7, "Toyota","Supra","1234567897")
+    };
+}
