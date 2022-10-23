e6f4d3af528489dfde6c0fae445d263ad3438427
rgruevski <robgruevski@gmail.com>
2022-10-23 12:58:25 -0400

    16. Understanding the MVVM Pattern

---
+++ ViewModels/BaseViewModel.cs
@@ -0,0 +1,10 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+namespace UdemyMAUICarListing.ViewModels;
+public partial class BaseViewModel : ObservableObject
+{
+    [ObservableProperty]
+    string _title;
+
+    [ObservableProperty]
+    bool _isBusy;
+}
